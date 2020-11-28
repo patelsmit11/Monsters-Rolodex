6ff8217651c148abb0993298d1081d444f02da66
patelsmit11 <patel.smit11@gmail.com>
2020-11-28 16:43:27 -0500

    Project - Initial Create

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+ import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
